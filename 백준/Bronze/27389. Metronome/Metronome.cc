10d63ed51c4b81e8e6e8736e193c34b9f101caa3
kangminhee <62800684+kangminhee@users.noreply.github.com>
2025-09-04 23:46:45 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    double res = n / 4.0;
+    cout << fixed << setprecision(2) << res << "\n";
+
+  return 0;
+}
