04736c1882745384317dc629423835c369ae95af
kangminhee <62800684+kangminhee@users.noreply.github.com>
2025-09-06 23:25:52 +0900

    [Bronze II] Title: 팰린드롬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    for (int i = 0, j = s.length()-1; i < j; i++, j--) {
+        if (s[i]!=s[j]) {
+            cout << "false";
+            return 0;
+        }
+    }
+    cout << "true";
+    return 0;
+}
